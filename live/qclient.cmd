4b4071943a36221101c6e7070f3847342d5a29a2
lemedege <lassegyrn@gmail.com>
2019-02-05 16:53:58 +0100

    added files

---
+++ live/qclient.cmd
@@ -0,0 +1,27 @@
+cam connect=smr8:24920
+cam kinect open
+cam poolget img=18 all
+cam gmkget
+cam connect=smr15:24920
+cam poolget img=18 fmt="rgb" all
+kinect open
+cam kinect open
+cam poolget img=18 fmt="rgb" all
+cam connect=smr15:24920
+cam poolget img=18 fmt="rgb" all
+cam connect=smr15:24920
+cam poologet img=18 all
+cam poolget img=18 all
+cam poolget img=18 fmt="rgb" all
+cam connect=smr15:24920
+cam poolget img=18 fmt="rgb" all
+poolget help
+cam poolget img=18 fmt=rgb all
+cam poolget img=18 fmt=RGB all
+cam connect=smr15:24920
+cam poolget img=18 fmt=rgb all
+cam poolget img=18  all
+cam poolget img=18  fmt=rgb all
+cam gmkget
+cam poolget img=18  fmt=rgb all
+cam gmkget
